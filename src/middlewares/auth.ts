54dad5089db15a6060a60685d9b4bfcfcb8d7ef6
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2026-02-21 22:26:19 +0600

    added auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import { JwtPayload } from "./../../node_modules/@types/jsonwebtoken/index.d";
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import config from "../config";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req?.headers.authorization;
+      if (!token) {
+        throw new Error("Unauthorized Access!!");
+      }
+      const decoded = jwt.verify(token, config.jwt_secret!) as JwtPayload;
+
+      if (roles?.length && !roles.includes(decoded?.role)) {
+        throw new Error("Unauthorized Access!!");
+      }
+
+      req.user = decoded;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
